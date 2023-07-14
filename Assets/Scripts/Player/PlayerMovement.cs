3e498619c5c8c77aa711aa3000c1cb4525160607
UltimateDecaf <ultimatedecaf@gmail.com>
2023-07-14 15:28:21 +0400

    smoothed movement, speed slowed

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -8,12 +8,14 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rigidbody;
     [SerializeField] private float speed;
-    private Vector2 movementVector;
+    private Vector2 smoothedMoveVector;
+    private Vector2 currentVelocity;
     Quaternion rotation;
 
     public InputActionAsset playerInput;
     public InputAction moveAction;
-    public InputAction lookAction;
+
+
     public Vector3 mousePos;
     private Camera mainCamera;
     private Vector3 objectPosition;
@@ -22,6 +24,7 @@ public class PlayerMovement : MonoBehaviour
     {
         moveAction = playerInput.FindActionMap("Player").FindAction("Move");
 
+
         rigidbody = GetComponent<Rigidbody2D>();
         rotation = Quaternion.identity;
         mainCamera = Camera.main;
@@ -29,8 +32,8 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        MovePlayer();
-        RotateToCursorPosition();
+       MovePlayer();
+       RotateToCursorPosition();
 
     }
 
@@ -44,15 +47,17 @@ public class PlayerMovement : MonoBehaviour
         mousePos.y = mousePos.y - objectPosition.y; 
 
         float angleToRotate = (Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg) - 90;
-        
         transform.rotation = Quaternion.Euler(new Vector3(0,0,angleToRotate));
 
-    }
+    } 
 
     private void MovePlayer()
     {
         Vector2 moveVector = moveAction.ReadValue<Vector2>();
-        rigidbody.velocity = moveVector * speed;
+        smoothedMoveVector = Vector2.SmoothDamp(smoothedMoveVector, moveVector, ref currentVelocity, 0.2f);
+        rigidbody.velocity = smoothedMoveVector * speed;
+
     }
+
  
 }
