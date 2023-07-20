10c4cc207aedc74415309ec43c764875c5eef479
UltimateDecaf <ultimatedecaf@gmail.com>
2023-07-20 14:11:32 +0400

    shooting not working properly

--- Assets/Scripts/Player/PlayerShoot.cs
+++ Assets/Scripts/Player/PlayerShoot.cs
@@ -10,15 +10,28 @@ public class PlayerShoot : MonoBehaviour
 
     public InputActionAsset playerInput;
     public InputAction fireAction;
+
+    private Rigidbody2D rigidbody;
     // Start is called before the first frame update
     void Start()
     {
-         
+        fireAction = playerInput.FindActionMap("Player").FindAction("Fire");
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (fireAction.triggered)
+        {
+            Fire();
+        }
+    }
+
+    public void Fire()
+    {
+        Instantiate(bullet, this.transform.position, Quaternion.identity, this.transform);
+        rigidbody.velocity = new Vector2(transform.position.x * speed, transform.position.y * speed);
+
     }
 }
