c8dee746382d42db50f805f5eeecc71969c4949a
UltimateDecaf <ultimatedecaf@gmail.com>
2023-08-04 16:03:09 +0400

    score is shown in the game over screen

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,19 +10,24 @@ public class GameManager : MonoBehaviour
 
     [SerializeField] private TextMeshProUGUI scoreUI;
     private int score;
-
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject gameOverScreen;
+    [SerializeField] private TextMeshProUGUI finalScore;
+    private Health playerHealth;
 
     void Awake()
     {
         Instance = this;
         score = 0;
         UpdateScoreUI();
+        playerHealth = player.GetComponent<Health>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CheckPlayerHealth(playerHealth.GetHealth());
     }
 
     public void AddScore(int scoreToAdd)
@@ -34,5 +39,14 @@ public class GameManager : MonoBehaviour
     void UpdateScoreUI()
     {
         scoreUI.text = "Score: " + score;
+    }
+
+    private void CheckPlayerHealth(int currentHealth)
+    {
+        if(currentHealth <= 0)
+        {
+            gameOverScreen.SetActive(true);
+            finalScore.text = "Final score: " + score; 
+        }
     }
 }
