d6cb4803679d370cd564b04012209547d01e5420
UltimateDecaf <ultimatedecaf@gmail.com>
2023-08-09 14:42:22 +0400

    fixed bullet destruction logic

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -18,7 +18,7 @@ public class Bullet : MonoBehaviour
 
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(UnityEngine.Collider2D collision)
     {
         if (!(collision.gameObject.CompareTag("Player")))
         {
@@ -26,32 +26,8 @@ public class Bullet : MonoBehaviour
             affectedGameObject.SendMessage("GiveDamage", 10);
             int scoreToAdd = Random.Range(5, 11);
             GameManager.Instance.AddScore(scoreToAdd);
-            //changes the color when the bullet hits the object
-            SpriteRenderer spriteRenderer = affectedGameObject.GetComponent<SpriteRenderer>();
-            spriteRenderer.color = Color.white;
-            Debug.Log(spriteRenderer.color);
-
-
-
-        } else if (collision.gameObject.CompareTag("GameBoundary"))
-        {
             Destroy(this.gameObject);
-        }
+        } 
 
-    }
-
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (!(collision.gameObject.CompareTag("Player"))) 
-        {
-           GameObject affectedGameObject = collision.gameObject;
-
-           //changes the color when the bullet hits the object
-           SpriteRenderer spriteRenderer = affectedGameObject.GetComponent<SpriteRenderer>();
-           spriteRenderer.color = new Color32(166, 7, 7, 255);
-            Destroy(this.gameObject);
-            Debug.Log(spriteRenderer.color);
-        }
-    
     }
 }
