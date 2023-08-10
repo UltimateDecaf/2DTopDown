7e48e2119293ef7afbb49b8db8142eba1230e77c
UltimateDecaf <ultimatedecaf@gmail.com>
2023-08-10 20:11:25 +0400

    continued work on input

---
+++ Assets/Scripts/Input/InputReader.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Input Reader", menuName = "Input/Input Reader")]
+public class InputReader : ScriptableObject
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
