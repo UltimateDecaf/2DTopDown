f10071f821f350b49814f767b95f69e5d7961950
UltimateDecaf <ultimatedecaf@gmail.com>
2023-08-25 00:49:31 +0400

    enemy spawn works fine now

--- Assets/Scripts/Gameplay/Enemy/EnemySpawn.cs
+++ Assets/Scripts/Gameplay/Enemy/EnemySpawn.cs
@@ -7,18 +7,28 @@ public class EnemySpawn : NetworkBehaviour
 {
     [SerializeField] private GameObject enemyPrefab;
     private Vector3[] spawnPositions = new Vector3[] { new Vector3(-43, -45, 0), new Vector3(0, -49, 0), new Vector3(-45.8f, -45.4f, 0), new Vector3(42.9f, -44.3f, 0) };
-    [SerializeField] private float spawnTime = 3.0f;
-    [SerializeField] private float subtractionTime = 0.1f;
-    private void Update()
+    [SerializeField] private float spawnTime = 10.0f;
+    [SerializeField] private float subtractionTime = 0.01f;
+
+    public override void OnNetworkSpawn()
     {
-        Invoke("SpawnEnemy", spawnTime);
+        InvokeRepeating("SpawnEnemy", 0.5f, spawnTime);
     }
+  
     void SpawnEnemy()
     {
         if (!IsServer) return;
         int spawnIndex = Random.Range(0, spawnPositions.Length);
         GameObject enemy = Instantiate(enemyPrefab, spawnPositions[spawnIndex], Quaternion.identity);
         enemy.GetComponent<NetworkObject>().Spawn();
-        spawnTime = spawnTime - subtractionTime;
+        if(spawnTime > 2f)
+        {
+            spawnTime -= subtractionTime;
+        }
+        else
+        {
+            spawnTime = 3f;
+        }
+    
     }
 }
--- Assets/Scripts/Gameplay/Systems/MeleeDamageOnContact.cs
+++ Assets/Scripts/Gameplay/Systems/MeleeDamageOnContact.cs
@@ -9,7 +9,7 @@ public class MeleeDamageOnContact : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.rigidbody != null)
+        if (collision.rigidbody != null && collision.gameObject.tag.Equals("Player"))
         { 
             if (collision.rigidbody.TryGetComponent<Health>(out Health health))
             {
